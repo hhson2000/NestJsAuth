a98f3ee4652f86b15ed7500de83843c809836ede
HoangHaiSon <frchoanghaison@gmail.com>
2023-08-08 10:10:57 +0700

    edit expries time

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -62,7 +62,7 @@ export class AuthService {
     const secret = this.config.get('JWT_SECRET');
 
     const token = await this.jwt.signAsync(payload, {
-      expiresIn: '60m',
+      expiresIn: '59m',
       secret: secret,
     });
 
